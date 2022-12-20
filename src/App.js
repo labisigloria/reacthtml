9156ded3388a1a6f1928a59e6d42bf346428477c
Olabisi.Adekoya <gloryolabisi.go@gmail.com>
2022-12-19 22:49:28 -0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+// import Popup from "./components/Popup";
+import Dashboard from "./pages/dashboard";
+import Nfts from "./pages/Nfts";
+import Swap from "./pages/Swap";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <>
+              
+              <Dashboard />
+              
+            </>
+          }
+        />
+        <Route
+          path="/Swap"
+          element={
+            <>
+              <Swap />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/Nfts"
+          element={
+            <>
+              <Nfts />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+      {/* <Home /> */}
+      {/* <Dashboard /> */}
+      {/* <Swap /> */}
+      {/* <Nfts /> */}
+    </>
   );
 }
 
 export default App;
+
